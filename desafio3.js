42584aa4bccc7fc2a607420a5a1bbb52175bbde0
daniel-rodri <162440520+daniel-rodri@users.noreply.github.com>
2024-04-18 08:40:35 -0300

    Add files via upload

---
+++ desafio3.js
@@ -0,0 +1,13 @@
+console.log = [10,6,8,5,5,10]
+
+console.log9notas //todo o array
+
+notas.pop();
+
+console.log(notas) //array sem o ultimo numero
+
+const media = (notas[0]+ notas[1]+ notas[2]+ notas[3])/ notas.length;
+
+console.log(`A media é ${media}.`)
+
+// o metodo pop () remove o ultimo elemento do seu array
