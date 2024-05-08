b25fb6c4ba19a9be8579af851527ab324e86d67a
daniel-rodri <162440520+daniel-rodri@users.noreply.github.com>
2024-05-08 12:01:25 -0300

    Add files via upload

---
+++ desafio6.js
@@ -0,0 +1,8 @@
+const salaJS = ["evaldo","camis","mari"];
+const salaPython = ["ju","leo","raquel"];
+
+const salasUnificadas = salaJS.concat(salaPython);
+const salasUnificadas2 = salaPython.concat(salaJS);
+
+console.log(salasUnificadas)
+console.log(salasUnificadas2)
---
+++ desafio7.js
@@ -0,0 +1,9 @@
+const alunos = ["joao","juliana","ana","caio"];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEmedias = [alunos,medias]
+
+console.log(
+    `A aluna da posiçao 1 da lista de alunos e:${listaDeAlunosEmedias[0][1]}
+     A nota dessa aluna e:${listaDeAlunosEmedias[1][1]}`
+);
