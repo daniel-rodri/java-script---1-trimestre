9309a707a6fd996b4b79da7599af0ae5e8707895
daniel-rodri <162440520+daniel-rodri@users.noreply.github.com>
2024-05-08 12:00:58 -0300

    Add files via upload

---
+++ desafio5.js
@@ -0,0 +1,7 @@
+const nomes = ["joao","ana","caio","lara","marjorie","leo"]
+                //0    //1   //2    //3      //4      //5 
+nomes.splice(1,2, "rodrigo")//(indice,quantidade de elementos,
+                                //o que deseja colocar)
+//nomes.push("rodrigo")
+
+console.log(nomes)
